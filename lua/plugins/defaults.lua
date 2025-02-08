cf2a96aa4050b5ef84425fbc6e607268122d874f
Christoffer Berglund <christoffer.berglund@icloud.com>
2025-02-08 17:40:02 +0100

    Add new theme

--- lua/plugins/defaults.lua
+++ lua/plugins/defaults.lua
@@ -1,6 +1,13 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+if true then return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine-moon",
+    },
+  },
+} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
