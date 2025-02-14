e4a34a74d54253a355f15385e24c3c1078c4e677
Christoffer Berglund <christoffer.berglund@icloud.com>
2025-02-14 12:58:56 +0100

    Added neogit, mini.files and oil
    I want to test out neogit a bit, and i would like to change the file
    system from explorer to oil and mini.files.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>e", function()
+  require("mini.files").open()
+end, { desc = "Open mini.files" })
+
+vim.keymap.set("n", "<leader>gm", function()
+  require("neogit").open()
+end, { desc = "Open Neogit" })
